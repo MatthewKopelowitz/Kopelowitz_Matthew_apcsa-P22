ff61bfeca674693c544044cfc78ef94f598bb64d
MatthewKopelowitz <47190077+MatthewKopelowitz@users.noreply.github.com>
2019-02-01 11:08:08 -0800

    Finished Unit01

---
+++ Unit01/src/StarsAndStripes.java
@@ -0,0 +1,64 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      System.out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   System.out.println("********************");
+	   
+   }
+
+   public void printTwentyDashes()
+   {
+	   System.out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   System.out.println("\n\n\n");
+   }
+   
+   public void printASmallBox()
+   {	
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   
+   }
+ 
+   public void printABigBox()
+   { 
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+   }   
+}
---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,27 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      
+	   StarsAndStripes start = new StarsAndStripes();
+	   
+      //call the methods needed to make the patterns on the word document
+      
+	   start.printASmallBox();
+	   start.printTwoBlankLines();
+	   start.printABigBox();
+	   
+	   
+   }
+}
